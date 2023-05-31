93c3e1d32576d0a91b32564d840d871b0431abaf
Abhin <Dell@192.168.0.198>
2023-05-31 18:55:24 +0530

    Initial Commit

---
+++ ProjectTestNew/src/test/java/packs/Demo1.java
@@ -0,0 +1,38 @@
+package packs;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+/*		
+	1.Chrome
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+	2. Firefox
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+	3.Edge
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+	4.IE
+		WebDriver driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+				
+	
+*/		
+	
+
+	}
+
+}
